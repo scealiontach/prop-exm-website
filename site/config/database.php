af9897355e9e73164c3fa0c2371d78a4234397cc
Kevin O'Donnell <scealiontach@github.com>
2017-06-12 20:06:41 +0000

    fix syntax errors

--- site/config/database.php
+++ site/config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', getenv('service_propeller_address'),
+            'host' => env('DB_HOST', getenv('service_propeller_address')),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', getenv('service_propeller_dbname'),
-            'username' => env('DB_USERNAME', getenv('service_propeller_user'),
-            'password' => env('DB_PASSWORD', getenv('service_propeller_password'),
+            'database' => env('DB_DATABASE', getenv('service_propeller_dbname')),
+            'username' => env('DB_USERNAME', getenv('service_propeller_user')),
+            'password' => env('DB_PASSWORD', getenv('service_propeller_password')),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
